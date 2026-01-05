c60ff668ffc1a3dcd94ca37685d42630f0259132
ARPGC <ecobirlaapp@gmail.com>
2026-01-05 12:25:09 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "i.ibb.co" },
+      { protocol: "https", hostname: "placehold.co" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "api.qrserver.com" },
+    ],
+  },
+};
+
+module.exports = nextConfig;
