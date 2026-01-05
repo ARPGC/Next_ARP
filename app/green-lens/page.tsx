df5f819e34eb755c40ab4542632fe0c6298cf929
ARPGC <ecobirlaapp@gmail.com>
2026-01-05 12:54:56 +0530

    Create page.tsx

---
+++ app/green-lens/page.tsx
@@ -0,0 +1,10 @@
+import GreenLensFeed from "@/components/GreenLensFeed";
+import { Aperture } from "lucide-react";
+
+export default function GreenLensPage() {
+  return (
+    <div className="min-h-screen pb-24 lg:pb-0">
+      <GreenLensFeed />
+    </div>
+  );
+}
